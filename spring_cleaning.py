803d0227bbe9a8c982f6f31ae4b8bc6d4e10b505
Rene Cortez <rcortezk9@gmail.com>
2019-09-26 20:51:38 -0500

    Corrected issue with denied access.

--- spring_cleaning.py
+++ spring_cleaning.py
@@ -10,17 +10,17 @@ s3 = boto3.client('s3', region_name='us-east-1',
                   aws_secret_access_key=AWS_SECRET)
 
 # List only objects that start with '2018/final_'
-response = s3.list_objects(Bucket='gid-staging',
+response = s3.list_objects(Bucket='gim-staging',
                            Prefix='2018/final_')
 
 # Iterate over the objects
 if 'Contents' in response:
     for obj in response['Contents']:
         # Delete the object
-        s3.delete_object(Bucket='gid-staging', Key=obj['Key'])
+        s3.delete_object(Bucket='gim-staging', Key=obj['Key'])
 
 # Print the keys of remaining objects in the bucket
-response = s3.list_objects(Bucket='gid-staging')
+response = s3.list_objects(Bucket='gim-staging')
 
 for obj in response['Contents']:
     print(obj['Key'])
